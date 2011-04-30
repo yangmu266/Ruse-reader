4beeb11c5818cd019aec3b3f45265f3172b226fe
Your Name <tarlou@VMware-Mac.local>
2011-04-30 17:07:31 +0800

    new project

---
+++ Ruse-reader/Classes/Ruse_readerAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  Ruse_readerAppDelegate.h
+//  Ruse-reader
+//
+//  Created by tarlou on 11-4-30.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Ruse_readerAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
